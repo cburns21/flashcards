09f18a60da5a03f507d6faedee3db7093624f3ee
caroline burns <carolineburns@carolines-MacBook.local>
2019-01-23 10:04:22 -0700

    made input and submit field but data isn't working yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/card';
+import Data from './components/data';
+
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      flashcard: [],
+      id: 0,
+      name: '',
+      description: '',
+      example: '',
+      tags: [],
+      link: ''
+    }
+  }
+  
+  getRandomIntInclusive = (max) => {
+    const min = 0
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  generateFlashcard = (e) => {
+    const randomIndex = this.getRandomIntInclusive(this.state.data.length)
+    const randomCard = this.state.data[randomIndex]
+    this.setState({
+      name: randomCard.name,
+      description: randomCard.description,
+      tags: randomCard.tags,
+      link: randomCard.link
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="row navbar justify-content-end pb-5">
+          <ul className="nav">
+          </ul>
+        </div>
+        <div className="row justify-content-center py-5">
+          <div className="col-8 text-center">
+            <h1 className="pb-2">Flashcards!</h1>
+            <p className="pb-2">An app for studying common JavaScript methods.</p>
+            <div className="form-group">
+              <label for="exampleFormControlTextarea1">Type response here  </label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
+            </div>
+            <button className="btn btn-danger btn-lg" onClick={this.generateCard}>Submit</button>
+          </div>
+        </div> 
+        <div className="row justify-content-center">
+        {this.state.name
+          ? <Card
+          name= {this.state.name}
+          description={this.state.description}
+          tags={this.state.tags}
+          link={this.state.link}
+          /> 
+          : '' }
+          <Data></Data>
+        </div>
       </div>
     );
   }
